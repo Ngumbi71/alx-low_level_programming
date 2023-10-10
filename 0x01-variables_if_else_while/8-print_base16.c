848af2f4fca170604d924a06ddf046fb0c62430e
Ngumbi71 <sngumbi71@gmail.com>
2023-10-09 21:12:21 -0700

    update

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: each time return 0
+ */
+
+int main(void)
+{
+	char s;
+
+	int t;
+
+	s = 'a';
+	t = 0;
+	while
+		(t < 10) {
+			putchar(t + '0');
+			t++;
+		}
+	while
+		(s <= 'f') {
+			putchar(s);
+			s++;
+		}
+	putchar('\n');
+	return (0);
+}
