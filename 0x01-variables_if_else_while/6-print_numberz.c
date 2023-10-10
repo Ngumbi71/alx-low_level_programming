c29995e7f1d609cdccd8bdf3192ba2dd635a74c4
Ngumbi71 <sngumbi71@gmail.com>
2023-10-09 20:54:28 -0700

    update

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: eacH time returns 0
+ */
+
+int main(void)
+{
+	int z;
+
+	z = 0;
+	while
+		(z < 10) {
+			putchar(z + '0');
+			z++;
+		}
+	putchar('\n');
+	return (0);
+}
