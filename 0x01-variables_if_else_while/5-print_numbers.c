b7fe543afbfcbf6afd8b7c8e259b4113cd5d72ac
Ngumbi71 <sngumbi71@gmail.com>
2023-10-09 20:43:53 -0700

    updated

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,1 +1,15 @@
-i
+#include <stdio.h>
+/**
+ * main - entry
+ * Return: each time returns o
+ */
+
+int main(void)
+{
+	int k;
+
+	for (k = 0; k < 10; k++)
+		printf("%d", k);
+	putchar('\n');
+	return (0);
+}
