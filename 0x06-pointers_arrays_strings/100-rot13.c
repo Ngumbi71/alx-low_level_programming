b48b4c351ac1ad11adbef4faf58b45a43d8d2fa2
Ngumbi71 <sngumbi71@gmail.com>
2023-10-19 23:38:23 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * rot13 - encoder replaces a letter with the 13th letter
+ * after it in the latin alphabet
+ * @s: pointer to a string
+ * Return: *s
+ */
+char *rot13(char *s)
+{
+	int x;
+	int y;
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (x = 0; s[x] != '\0'; x++)
+		{
+			for (y = 0; y < 52; y++)
+			{
+				if (s[x] == data1[y])
+				{
+				s[x] = datarot[y];
+				break;
+				}
+			}
+		}
+	return (s);
+}
+
